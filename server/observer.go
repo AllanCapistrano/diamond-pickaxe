e53f6da35c3bd8691c84e5d1ddb1153734bcc527
AllanCapistrano <allan.capistrano3@gmail.com>
2025-11-09 11:40:59 -0300

    chore: moving the settings setup to the Loop function

--- server/observer.go
+++ server/observer.go
@@ -2,31 +2,72 @@ package server
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"gihub.com/allancapistrano/diamond-pickaxe/cmd"
 	"gihub.com/allancapistrano/diamond-pickaxe/handler"
+	"gihub.com/allancapistrano/diamond-pickaxe/settings"
 )
 
 // Check if there are files in the vault to synchronize
-func Loop(vaultPath string, sleep int) {
+func Loop() {
 	for {
-		if handler.HasFilesToDownload(vaultPath) {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal("Couldn't open the user home directory.")
+		}
+
+		settingsFilePath := fmt.Sprintf(
+			"%s/%s/%s",
+			homeDir,
+			settings.CONFIG_DIRECTORY_NAME,
+			settings.DIAMOND_PICKAXE_DIRECTORY_NAME,
+		)
+
+		if !settings.CheckSettingsFileExists() {
+			settings.CreateSettingsFile(settings.Settings{
+				VaultPath:       "",
+				VaultRepository: "",
+				SyncInSeconds:   30,
+			})
+
+			fmt.Printf("The settings file was not found. But we created one at '%s'\n", settingsFilePath)
+			fmt.Printf("Open the file '%s' to set up the configurations\n", settings.SETTINGS_FILE_NAME)
+			os.Exit(1)
+		}
+
+		diamondPickaxeSettings := settings.LoadSettingsFile()
+
+		if !settings.IsVaultPathValid(diamondPickaxeSettings.VaultPath) {
+			fmt.Printf("The vault path is invalid\n")
+			fmt.Printf("Open the file '%s/%s' to set up the configurations\n", settingsFilePath, settings.SETTINGS_FILE_NAME)
+			os.Exit(1)
+		}
+
+		if !settings.IsVaultRepositoryValid(diamondPickaxeSettings.VaultRepository) {
+			fmt.Printf("The vault repository is invalid\n")
+			fmt.Printf("Open the file '%s/%s' to set up the configurations\n", settingsFilePath, settings.SETTINGS_FILE_NAME)
+			os.Exit(1)
+		}
+
+		if handler.HasFilesToDownload(diamondPickaxeSettings.VaultPath) {
 			fmt.Println("There are files to download!")
 
-			getChanges(vaultPath)
+			getChanges(diamondPickaxeSettings.VaultPath)
 		} else { // TODO: Rever lógica, pois talvez isso possa causar conflitos
-			if handler.HasFilesToSubmit(vaultPath) {
+			if handler.HasFilesToSubmit(diamondPickaxeSettings.VaultPath) {
 				fmt.Println("There are files to submit!")
 
-				submitChanges(vaultPath)
+				submitChanges(diamondPickaxeSettings.VaultPath)
 			} else {
 				fmt.Println("There are no files to submit!")
 			}
 		}
 
-		time.Sleep(time.Duration(sleep) * time.Second)
-
+		time.Sleep(time.Duration(diamondPickaxeSettings.SyncInSeconds) * time.Second)
 	}
 }
 
