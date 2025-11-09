3f7ca8cea2a32324b3c894dad6dae14d3684bac9
AllanCapistrano <allan.capistrano3@gmail.com>
2025-11-09 09:55:34 -0300

    chore: creating function to check if the vault repository URL is valid

--- settings/value.go
+++ settings/value.go
@@ -2,13 +2,28 @@ package settings
 
 import (
 	"log"
+	"net/url"
 	"os"
 )
 
 // Check if the vault path is valid
 func IsVaultPathValid(vaultPath string) bool {
 	if _, err := os.Stat(vaultPath); os.IsNotExist(err) {
-		log.Fatalf("Vault path '%s' is invalid.", vaultPath)
+		log.Fatalf("Vault path '%s' is invalid", vaultPath)
+	}
+
+	return true
+}
+
+// Check if the vault repository is valid
+func IsVaultRepositoryValid(vaultRepository string) bool {
+	parsed, err := url.ParseRequestURI(vaultRepository)
+	if err != nil {
+		log.Fatalf("Vault repository '%s' is invalid", vaultRepository)
+	}
+
+	if parsed.Scheme == "" || parsed.Host == "" {
+		log.Fatalf("Vault repository '%s' is invalid", vaultRepository)
 	}
 
 	return true
