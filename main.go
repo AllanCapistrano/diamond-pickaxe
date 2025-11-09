6c28d9487cc70ef3e75b178c895aa089711593e4
AllanCapistrano <allan.capistrano3@gmail.com>
2025-11-09 11:25:52 -0300

    refactor: using logrus instead of the default log library

--- main.go
+++ main.go
@@ -5,11 +5,28 @@ import (
 	"log"
 	"os"
 
+	"gihub.com/allancapistrano/diamond-pickaxe/logger"
 	"gihub.com/allancapistrano/diamond-pickaxe/server"
 	"gihub.com/allancapistrano/diamond-pickaxe/settings"
 )
 
+const APP_NAME = "diamond-pickaxe"
+
 func main() {
+	logger.Init(APP_NAME)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Couldn't open the user home directory.")
+	}
+
+	settingsFilePath := fmt.Sprintf(
+		"%s/%s/%s",
+		homeDir,
+		settings.CONFIG_DIRECTORY_NAME,
+		settings.DIAMOND_PICKAXE_DIRECTORY_NAME,
+	)
+
 	if !settings.CheckSettingsFileExists() {
 		settings.CreateSettingsFile(settings.Settings{
 			VaultPath:       "",
@@ -17,21 +34,8 @@ func main() {
 			SyncInSeconds:   30,
 		})
 
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal("Couldn't open the user home directory.")
-		}
-
-		settingsFilePath := fmt.Sprintf(
-			"%s/%s/%s",
-			homeDir,
-			settings.CONFIG_DIRECTORY_NAME,
-			settings.DIAMOND_PICKAXE_DIRECTORY_NAME,
-		)
-
 		fmt.Printf("The settings file was not found. But we created one at '%s'\n", settingsFilePath)
 		fmt.Printf("Open the file '%s' to set up the configurations\n", settings.SETTINGS_FILE_NAME)
-
 		os.Exit(1)
 	}
 
@@ -39,13 +43,13 @@ func main() {
 
 	if !settings.IsVaultPathValid(diamondPickaxeSettings.VaultPath) {
 		fmt.Printf("The vault path is invalid\n")
-		fmt.Printf("Open the file '%s' to set up the configurations\n", settings.SETTINGS_FILE_NAME)
+		fmt.Printf("Open the file '%s/%s' to set up the configurations\n", settingsFilePath, settings.SETTINGS_FILE_NAME)
 		os.Exit(1)
 	}
 
 	if !settings.IsVaultRepositoryValid(diamondPickaxeSettings.VaultRepository) {
 		fmt.Printf("The vault repository is invalid\n")
-		fmt.Printf("Open the file '%s' to set up the configurations\n", settings.SETTINGS_FILE_NAME)
+		fmt.Printf("Open the file '%s/%s' to set up the configurations\n", settingsFilePath, settings.SETTINGS_FILE_NAME)
 		os.Exit(1)
 	}
 
